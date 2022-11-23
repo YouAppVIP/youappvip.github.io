745fd72f169d7ffc4f1627e87437aaf3841c303d
Daniel Davis <daniel@youapp.vip>
2022-11-23 04:00:25 -0800

    logging on form

--- leads_form.js
+++ leads_form.js
@@ -11,14 +11,17 @@ $('#leads-form').submit(function (e) {
     dataType: "json", //the standard data type for most ajax requests
     statusCode: { //the status code from the POST request
       0: function (data) { //0 is when Google gives a CORS error, don't worry it went through
+        console.log('form submitted');
         //success
         $('#form-success').text('Welcome!');
       },
       200: function (data) {//200 is a success code. it went through!
+        console.log('form submitted');
         //success
         $('#form-success').text('Welcome!');
       },
       403: function (data) {//403 is when something went wrong and the submission didn't go through
+        console.error('problem submitting form')
         //error
         alert('Oh no! something went wrong. we should check our code to make sure everything matches with Google');
       }
